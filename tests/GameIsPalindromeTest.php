54c5b81e995b1dbf6eeced81fb7d00b829643502
Aslan <autlevaslan@yandex.ru>
2022-06-26 16:27:40 +0300

    fix

---
+++ tests/GameIsPalindromeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use function GameIsPalindrome\Engine\isPalindrome;
+use function GameIsPalindrome\Engine\reverseStr;
+
+class GameIsPalindromeTest extends TestCase
+{
+    public function testReverseStr(): void
+    {
+        $this->assertEquals('радар', reverseStr('радар'));
+        $this->assertEquals('дед', reverseStr('дед'));
+    }
+
+    public function testIsPalindrome(): void
+    {
+        $this->assertTrue(isPalindrome('радар'));
+        $this->assertFalse(isPalindrome('сказка'));
+    }
+}
