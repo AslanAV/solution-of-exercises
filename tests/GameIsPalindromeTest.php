82bb3294c0c37988632d0d7d021689ba6a02dcad
Aslan <autlevaslan@yandex.ru>
2022-06-27 14:31:32 +0300

    add test Is Palindrome

--- tests/GameIsPalindromeTest.php
+++ tests/GameIsPalindromeTest.php
@@ -4,7 +4,9 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 
+use function GameIsPalindrome\Engine\getRandomWord;
 use function GameIsPalindrome\Engine\isPalindrome;
+use function GameIsPalindrome\Engine\isPositiveAnswer;
 use function GameIsPalindrome\Engine\reverseStr;
 
 class GameIsPalindromeTest extends TestCase
@@ -19,5 +21,31 @@ class GameIsPalindromeTest extends TestCase
     {
         $this->assertTrue(isPalindrome('радар'));
         $this->assertFalse(isPalindrome('сказка'));
+    }
+
+    public function testGetRandomWord(): void
+    {
+        $words = [
+            'радар',
+            'игра',
+            'дед',
+            'довод',
+            'доход',
+            'заказ',
+            'сказка'
+        ];
+        $this->assertContains(getRandomWord(), $words);
+        $this->assertContains(getRandomWord(), $words);
+        $this->assertContains(getRandomWord(), $words);
+        $this->assertContains(getRandomWord(), $words);
+    }
+
+    public function testIsPositiveAnswer(): void
+    {
+        $this->assertTrue(isPositiveAnswer('y'));
+        $this->assertFalse(isPositiveAnswer('n'));
+        $this->assertFalse(isPositiveAnswer('a'));
+        $this->assertFalse(isPositiveAnswer(''));
+        $this->assertFalse(isPositiveAnswer('NASDAQ'));
     }
 }
